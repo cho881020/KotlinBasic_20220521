bd719e88c917c1e413191ca98b1bf7120c2f0c37
조경진 <cho881020@gmail.com>
2022-05-21 10:40:02 +0900

    xml에 id부여 태그 > kt에서 가져다 사용하기  - 코틀린 확장팩 설치

--- app/src/main/java/com/neppplus/kotlinbasic_20220521/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20220521/MainActivity.kt
@@ -2,11 +2,21 @@ package com.neppplus.kotlinbasic_20220521
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLog.setOnClickListener {
+
+
+
+        }
 
     }
 }
